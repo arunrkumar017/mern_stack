807b01622980eed40b6bc5e446dee3eb6f214057
arunrkumar017 <arunrkumar368@gmail.com>
2026-08-14 14:03:51 +0530

    removed error from code

--- JAVASCRIPT/Loops/question2.js
+++ JAVASCRIPT/Loops/question2.js
@@ -1,7 +1,7 @@
-//detremine a number is even or odd
+//determine a number is even or odd
 const prompt = require("prompt-sync")();
-let num = prompt("Enter your number : ");
-if(((num/2)*2)===num){ // alternate condition num%2 == 0  
+let num = Number(prompt("Enter your number : "));
+if(num%2 === 0){
     console.log("Number is Even");
 }else{
     console.log("Number is Odd");
